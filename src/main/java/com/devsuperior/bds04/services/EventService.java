ae5ae2ddc768b21ed36db4d205af660d9b4fa7e9
helderargomes <helderargomes@gmail.com>
2023-05-06 22:11:41 -0300

    EventControllerIT

--- src/main/java/com/devsuperior/bds04/services/EventService.java
+++ src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -3,6 +3,8 @@ package com.devsuperior.bds04.services;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +20,23 @@ public class EventService {
 	@Autowired
 	private EventRepository repository;
 
+	@Transactional(readOnly = true)
+	public Page<EventDTO> findAllPaged(Pageable pageable) {
+		Page<Event> list = repository.findAll(pageable);
+		return list.map(x -> new EventDTO(x));
+	}
+	
+	@Transactional
+	public EventDTO insert(EventDTO dto) {
+		Event event = new Event();
+		event.setName(dto.getName());
+		event.setDate(dto.getDate());
+		event.setUrl(dto.getUrl());
+		event.setCity(new City(dto.getCityId(), null));
+		repository.save(event);
+		return new EventDTO(event);
+	}
+	
 	@Transactional
 	public EventDTO update(Long id, EventDTO dto) {
 		try {
